15c8d15ea10945bf95c293490d822c81f3ec7194
Toni Rämö <toni.ramo@helsinki.fi>
2020-12-07 18:28:00 +0200

    vk6t5: Improve QueryBuilder

--- viikko6/tehtavat2-5/QueryLanguage/src/main/java/statistics/QueryBuilder.java
+++ viikko6/tehtavat2-5/QueryLanguage/src/main/java/statistics/QueryBuilder.java
@@ -12,34 +12,44 @@ import statistics.matcher.*;
  * @author toniramo
  */
 public class QueryBuilder {
+
     private Matcher matcher;
-    
+
     public QueryBuilder() {
-        matcher = new All();
+        this.initialize();
     }
-    
+
     public Matcher matcher() {
         return matcher;
     }
-    
+
     public QueryBuilder hasAtLeast(int value, String category) {
         this.matcher = new And(matcher, new HasAtLeast(value, category));
         return this;
     }
-    
+
     public QueryBuilder hasFewerThan(int value, String category) {
         this.matcher = new And(matcher, new HasFewerThan(value, category));
         return this;
     }
-    
+
     public QueryBuilder playsIn(String team) {
         this.matcher = new And(matcher, new PlaysIn(team));
         return this;
     }
-    
-    public Matcher build() {
-        return this.matcher;
+
+    public QueryBuilder oneOf(Matcher m1, Matcher m2) {
+        this.matcher = new Or(m1, m2);
+        return this;
     }
-    
-    
+
+    public Matcher build() {
+        Matcher m = this.matcher;
+        this.initialize();
+        return m;
+    }
+
+    private void initialize() {
+        this.matcher = new All();
+    }
 }
